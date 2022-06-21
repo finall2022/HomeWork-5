99db5d0910e75ba892ddffa90fece2fb11c86721
Aleks Fin <finall.git2022@gmail.com>
2022-06-21 16:42:03 +0300

    решена задача на определение палиндрома

--- sem2/Program.cs
+++ sem2/Program.cs
@@ -3,48 +3,57 @@
 палиндромом.
  */
 
+/* Уважаемый Владимир. Спасибо за семинары и обратную связь. 
+А вот эта задача очень помогла разобраться и с массивами
+и особенно с функциями, т.к. обычно спотыкался именно на них.
+по моему всё работает :) может, конечно, не совсем красиво.
+ */
 //функция для определения палиндрома
 int Pal(int[] arrTemp)
 {
     int i = 0;
     int count = 0;
     int lenghtTemp = arrTemp.Length;
-    Console.WriteLine(lenghtTemp);
     while (i < lenghtTemp)
         if (arrTemp[i] == arrTemp[lenghtTemp - 1])
         {
+            //Console.WriteLine($"i = {i}, lenghtTemp = {lenghtTemp}, count = {count}");
             i++;
             lenghtTemp--;
-            count++;
-            Console.WriteLine($"i = {i}, lenghtTemp = {lenghtTemp}, count = {count}");
+        }
+        else
+        {
+            count++;  //счетчик увеличивается, если условие не выполнено, т.е. одна из пар цифр не совпала
+            return count;
         }
     return count;
 }
+
 Console.Write("Введите число: "); //вводим число
 string istring = Console.ReadLine(); //считываем число
 int inum = int.Parse(istring);
 int lenght = istring.Length; //определяем размер массива
 int[] arr = new int[lenght]; //инициализируем массив с размером lenght
 int index = lenght - 1;
-Console.WriteLine($"Число состоит из {lenght} разрядов");
+/* Console.WriteLine($"Число состоит из {lenght} разрядов");
 Console.WriteLine($"Ц(и)");
-Console.WriteLine($"====");
-while (index >= 0 && index < lenght) //заполняем массив цифрами числа
+Console.WriteLine($"===="); */
+
+while (index >= 0 && index < lenght) //заполняем массив 
 {
     arr[index] = inum % 10;
-
     inum = inum / 10;
-
-    Console.WriteLine($"{arr[index]}({index})");
+    //Console.WriteLine($"{arr[index]}({index})");
     index--;
 }
 
-int c = Pal(arr);
-if (c % 2 == 0)
+int c = Pal(arr); /*вызываем функцию, передаем массив в виде аргумента
+получаем результат в переменную */
+if (c == 0)
 {
-    Console.Write("палиндром");
+    Console.Write($"Число {istring} является палиндромом");
 }
 else
 {
-    Console.Write("не палиндром");
+    Console.Write($"Число {istring} не является палиндромом");
 }
